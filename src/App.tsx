37043143c3a0b5fe66dce3e4f024297bdd77028b
shinysource <highly.flores@gmail.com>
2022-06-12 18:00:57 -0700

    Updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/main/Home'
+import GetStartedPage from './pages/main/GetStarted'
 import SignupPage from './pages/auth/Signup'
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<SignupPage />}></Route>
-      <Route path="/registration" element={<SignupPage />}></Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/getstarted" element={<GetStartedPage />}></Route>
+        <Route path="/registration" element={<SignupPage />}></Route>
+      </Routes>
+    </>
   )
 }
 
