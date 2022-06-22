c67965465ce9e3f2e79054ae079911326bb15396
shinysource <highly.flores@gmail.com>
2022-06-22 12:02:46 -0700

    Updated whitelist page ui

--- src/pages/auth/Verify.tsx
+++ src/pages/auth/Verify.tsx
@@ -48,7 +48,7 @@ const Signup = () => {
     },
     uiConf: {
       customCssStr:
-        ':root {\n  --black: #000000;\n   --grey: #F5F5F5;\n  --grey-darker: #B2B2B2;\n  --border-color: #DBDBDB;\n}\n\np {\n  color: var(--black);\n  font-size: 16px;\n  line-height: 24px;\n}\n\nsection {\n  margin: 40px auto;\n}\n\ninput {\n  color: var(--black);\n  font-weight: 600;\n  outline: none;\n}\n\nsection.content {\n  background-color: var(--grey);\n  color: var(--black);\n  padding: 40px 40px 16px;\n  box-shadow: none;\n  border-radius: 6px;\n}\n\nbutton.submit,\nbutton.back {\n  text-transform: capitalize;\n  border-radius: 6px;\n  height: 48px;\n  padding: 0 30px;\n  font-size: 16px;\n  background-image: none !important;\n  transform: none !important;\n  box-shadow: none !important;\n  transition: all 0.2s linear;\n}\n\nbutton.submit {\n  min-width: 132px;\n  background: none;\n  background-color: var(--black);\n}\n\n.round-icon {\n  background-color: var(--black) !important;\n  background-image: none !important;\n}'
+        ':root {\n  --black: #000000;\n   --white: #FFFFFF;\n  --grey-darker: #B2B2B2;\n  --border-color: #DBDBDB;\n}\n\np {\n  color: var(--black);\n  font-size: 16px;\n  line-height: 24px;\n}\n\nsection {\n  margin: 40px auto;\n}\n\ninput {\n  color: var(--black);\n  font-weight: 600;\n  outline: none;\n}\n\nsection.content {\n  background-color: var(--white);\n  color: var(--black);\n  padding: 40px 40px 16px;\n  box-shadow: none;\n  border-radius: 6px;\n}\n\nbutton.submit,\nbutton.back {\n  text-transform: capitalize;\n  border-radius: 6px;\n  height: 48px;\n  padding: 0 30px;\n  font-size: 16px;\n  background-image: none !important;\n  transform: none !important;\n  box-shadow: none !important;\n  transition: all 0.2s linear;\n}\n\nbutton.submit {\n  min-width: 132px;\n  background: none;\n  background-color: var(--black);\n}\n\n.round-icon {\n  background-color: var(--black) !important;\n  background-image: none !important;\n}'
     },
     onError: (error: String) => {
       console.error('WebSDK onError', error)
@@ -89,7 +89,7 @@ const Signup = () => {
   }
 
   return (
-    <div className="py-9 signup-back">
+    <div className="py-9 signup-back text-black">
       <Grid
         item
         container
@@ -126,16 +126,12 @@ const Signup = () => {
               md={6}
               lg={4}
             >
-              <Grid item className="text-[32px] font-bold text-white">
+              <Grid item className="text-[32px] font-bold">
                 <div className="font-podium49">WHITELIST</div>
               </Grid>
-              <Grid
-                item
-                className="text-base font-normal text-center text-white"
-                xs={12}
-              >
+              <Grid item className="text-base font-normal text-center" xs={12}>
                 <div className="mb-[8px]">
-                  Know your client, Anti money laundering with Sumsub{' '}
+                  Know Your Customer/Anti Money Laundering check with SumSub
                 </div>
               </Grid>
               <Grid item xs={12}>
