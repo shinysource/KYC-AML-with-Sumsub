a755d8625313977f85c780152e3eab114b43217e
shinysource <highly.flores@gmail.com>
2022-06-23 06:29:38 -0700

    Updated default port in env

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+process.env = { ...process.env, ...loadEnv('development', process.cwd()) }
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()]
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    port: parseInt(process.env.VITE_PORT ? process.env.VITE_PORT : '')
+  }
 })
